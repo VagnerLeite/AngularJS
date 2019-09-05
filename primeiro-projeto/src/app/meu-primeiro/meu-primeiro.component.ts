ac1fde7e85e11502b7c2502528156d5899f35f4d
Vagner <vagner@vagner.leite>
2019-09-05 16:31:35 -0300

    Primeiro Commit

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu Primeiro component com AngularJS</p>
+    `
+})
+
+export class MeuPrimeiroComponent {
+
+}
